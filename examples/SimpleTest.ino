21b91a9bf7c68b4988cfae0db668bd4692304d29
Jorge Villalobos <jorgemvc@gmail.com>
2018-04-19 19:26:16 -0600

    Create SimpleTest.ino

---
+++ examples/SimpleTest.ino
@@ -0,0 +1,29 @@
+/** @file    SimpleTest.h
+ *  @author  Jorge Villalobos Carvajal (jorgemvc@gmail.com)
+ *  @date    18.Abr.2018  
+ *  @version 1.0 
+ *  
+ *  @brief   Libray for easy usage of MotoMama H-Bridge.
+ *
+ *  @licence  MIT License
+ */
+// ---- Libraries
+#include <MotoMamaLib.h>
+
+// ---- Variables
+MotoMamaLib Robot;
+
+void setup() {
+  Robot.begin();
+  Robot.setSpeed(175,175);
+}  // void setup
+
+void loop() {
+  Robot.forward(500);
+  Robot.left(500);
+  Robot.backward(500);
+  Robot.right(500); 
+  Robot.stop();
+  
+  delay(1000);
+}  // void loop
